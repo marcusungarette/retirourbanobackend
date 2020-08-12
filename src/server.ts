99aed9740a609cd195073a2bca5ad451b64dbcff
marcusungarette <marcusungarette@gmail.com>
2020-08-12 07:35:29 -0300

    Starts DDD and TDD  Arquitecture from here

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
 import routes from './routes';
+import AppError from './errors/AppError';
 
 import uploadConfig from './config/upload';
 import './database';
@@ -8,8 +11,26 @@ import './database';
 const app = express();
 const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  // Caso um erro que foi setado detro da minha app
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+  // Caso erro inesperado
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
 
 app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
