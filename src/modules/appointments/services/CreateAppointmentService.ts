5ce4b82a6669229590f59cc2b788aa32ae7b598d
marcusungarette <marcusungarette@gmail.com>
2020-08-22 17:37:40 -0300

    Dependency Inversion Principle applied on appointments services

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,9 +1,10 @@
 import { startOfHour } from 'date-fns';
-import { getCustomRepository } from 'typeorm';
+
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
-import AppointmentsRepository from '../infra/typeorm/repositories/AppointmentsRepository';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
 
 interface RequestDTO {
   providerId: string;
@@ -11,12 +12,12 @@ interface RequestDTO {
 }
 
 class CreateAppointmentService {
-  public async execute({ providerId, date }: RequestDTO): Promise<Appointment> {
-    const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  constructor(private appointmentsRepository: IAppointmentsRepository) {}
 
+  public async execute({ providerId, date }: RequestDTO): Promise<Appointment> {
     const appointmentDate = startOfHour(date);
 
-    const findAppointmentInSameDate = await appointmentsRepository.findByDate(
+    const findAppointmentInSameDate = await this.appointmentsRepository.findByDate(
       appointmentDate,
     );
 
@@ -25,7 +26,7 @@ class CreateAppointmentService {
     }
 
     // Instancia o objeto criado porem nao salva no Database
-    const appointment = await appointmentsRepository.create({
+    const appointment = await this.appointmentsRepository.create({
       providerId,
       date: appointmentDate,
     });
