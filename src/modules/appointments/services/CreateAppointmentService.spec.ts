64b21e3663fc279cf923063133f7fed6e5429bf4
marcusungarette <marcusungarette@gmail.com>
2021-01-02 17:57:03 -0300

    Created codeCovarage report / created another test -> should not be able to create two appointments on the same time/date

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -14,5 +15,26 @@ describe('CreateAppointment', () => {
     });
 
     expect(appointment.providerId).toBe('123456789');
+  });
+
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointmentDate = new Date(2021, 0, 10, 11);
+
+    await createAppointment.execute({
+      date: appointmentDate,
+      providerId: '123456789',
+    });
+
+    expect(
+      createAppointment.execute({
+        date: appointmentDate,
+        providerId: '123456789',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
